a77d6c8b949823624ef593661c37ea33375d3c3c
DylanPiriou <dylan.piriou@hotmail.com>
2024-06-07 15:33:09 +0200

    feat: pages added + router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import User from './pages/User';
 
 function App() {
-
   return (
-    <>
-      <h1>Argent Bank</h1>
-    </>
-  )
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/user" element={<User />} />
+				<Route path="/*" element={<Login />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App
