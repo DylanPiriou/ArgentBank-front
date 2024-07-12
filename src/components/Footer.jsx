ea3eddbc87b6ffd844721550ba1b5bca20910275
DylanPiriou <dylan.piriou@hotmail.com>
2024-07-12 15:07:47 +0200

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-		<footer className='w-full flex items-center justify-center border border-t-gray py-8'>
+		<footer className='w-full flex items-center justify-center border-t border-t-gray py-8'>
 			<p>Copyright 2024 Argent Bank</p>
 		</footer>
 	);
